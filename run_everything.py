ffe42c48e24c66f3edf14ff3c8e7dd04b0acd290
alan woodhull <89449956+awood333@users.noreply.github.com>
2023-06-29 17:19:26 +0700

    x

--- run_everything.py
+++ run_everything.py
@@ -9,6 +9,7 @@ script_names = ['D:/Git_repos/bos/rawmilkupdate.py',
                 'D:/Git_repos/bos/wet_dry.py'                
                 ]
 # 'D:/Git_repos/bos/status_prep.py'
+#
 
 for script in script_names:
     subprocess.run(['python', script])
