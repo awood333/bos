5743029653ab04b0bb5a5712e31e7dd12cca73e2
alan woodhull <89449956+awood333@users.noreply.github.com>
2023-07-15 16:29:28 +0700

    x

--- status2.py
+++ status2.py
@@ -2,29 +2,68 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from datetime import date
-#
-f=pd.read_csv('F:\\COWS\\data\\milk_data\\fullday\\fullday.csv')
-milking= f.iloc[-3:,:-5].copy()
+# 
+import insem_ultra as iu
+import wet_dry as wd
+import inspect
+vname = [n for n, obj in inspect.getmembers(wd) if not inspect.ismodule(obj) ]
+# 
 
+bd=pd.read_csv('F:\\COWS\\data\\csv_files\\birth_death.csv',index_col=0,header=0,parse_dates=['birth_date','death_date'],low_memory=False,dtype=None)
+# 
+date_names = ['age cow','stop_last','lastcalf bdate','i_date','u_date','next bdate','ultra(e)']
+everything=pd.read_csv('F:\\COWS\\data\\insem_data\\all.csv',index_col=0,header=0,parse_dates=date_names, date_format='%m/%d/%Y',low_memory=False,dtype=None)
+# 
+f=pd.read_csv('F:\\COWS\\data\\milk_data\\fullday\\fullday.csv',parse_dates=['datex'])
+f1 = f.iloc[:,:-5].copy()       ##drops the last 5 columns - leaving datex and the WY_ids
+f2 = f.iloc[:,1:-5].copy()      #drops the datex and the last 5 columns - leaving only the WY_ids
+# 
+dd      = bd['death_date']
+fullday_datex = f['datex'].to_list()            #datex in list form
+f2cols  = [int(x) for x in f2.columns.tolist()] #list of WY_ids as integers
+# 
+# creates df for the past whole month with datex intact and a 'day' index.
+# 
+month_filter = 6
+year_filter  = 2023
+# 
+fx = f.loc[    (
+        (f['year'] == year_filter) & 
+        (f['month'] == month_filter)
+        ),    :    ].reset_index(drop=True)
+# 
 
-milking.set_index('datex',inplace=True)
-rng1 = milking.columns.to_list()
-rng = [x+1 for x in range(len(rng1))]
-i = 1
-j = 1
+col_integers = [int(x) for x in fx2.columns.to_list()]
+cols =col_integers          #200+ cols WY_ids
+rows1 = len(fx['datex'])     #30 rows    dates
+rows =+1
+maskmilk2 = []
+maskheif2 = []
+maskdry2  = []
 
-m4 = []
-while i < len(milking):
-    m3 = []
-    for j in rng:
-        # m1 =  milking.iloc[-j:,:].values
-        m1 =  milking.iloc[-j:,:]   
-        m1a = m1.T
-        # label = m1a[1].name
-        # m2 = [index for index , value in enumerate(m1.flatten()) if value>0]
-        m2 = m1.loc[(m1[1] > 0     )       ]
-        m3.append(m2)
-    m4.append(m3)
-    i += 1
-# m1 =  milking.iloc[-1:,:]
-m4
+# Loop through each element in fullday_wy
+for i in cols:
+    maskmilk1 = []
+    maskheif1 = []
+    maskdry1  = []
+    for j in range(rows):
+        r       = fx.iloc[j,i]
+        calf1   = iu.lb3a.iloc[j,1]
+        # fcalf2  = iu.lb3a.iloc[j,0]
+        daynum  = datex.iloc[j]
+        # t = fx.iloc[j,i]
+        maskmilk = r>0
+        maskheif = ((daynum < calf1) |( pd.isnull(calf1)))
+        maskmilk1.append(maskmilk)
+        maskheif1.append(maskheif)
+        j +=1
+        
+    maskmilk2.append(maskmilk1) 
+    maskheif2.append(maskheif1)   
+
+# maskmilk = pd.DataFrame({'alivemaskmilk': alivemaskmilk})
+# alivemaskmilk2
+# maskmilk2 = [[fx.iloc[j, i] > 0 for j in range(rows)] for i in cols]
+milking = pd.DataFrame(maskmilk2).T
+heifers = pd.DataFrame(maskheif2)
+# len(maskmilk2), len(cols),rows
