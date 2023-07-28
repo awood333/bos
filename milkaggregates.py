cc747c9343acf6910906fbf0503e30d4e49473e6
alan woodhull <89449956+awood333@users.noreply.github.com>
2023-07-28 13:56:44 +0700

    s

--- milkaggregates.py
+++ milkaggregates.py
@@ -13,14 +13,14 @@ bd      = pd.read_csv       ('F:\\COWS\\data\\csv_files\\birth_death.csv',      
 all     = pd.read_csv       ('F:\\COWS\\data\\insem_data\\all.csv',                              header=0)
 status  = pd.read_csv     ('F:\\COWS\\data\status\\status_column.csv',            index_col=0,header=0)       
 # 
-liters1_am = AM_liters
+liters1_am  = AM_liters
 wy1_am      = AM_wy
 liters1_pm  = PM_liters
 wy1_pm      = PM_wy
 # 
-wy=bd['WY_id']
-alive1=bd['death_date'].isnull()
-alive=wy.loc[alive1].copy()
+wy      = bd['WY_id']
+alive1  = bd['death_date'].isnull()
+alive   = wy.loc[alive1].copy()
 alive.reset_index(drop=True,inplace=True)
 #
 wy_am=wy1_am.           fillna(0).copy()
@@ -28,19 +28,20 @@ wy_pm=wy1_pm.           fillna(0).copy()
 liters_am=liters1_am.   fillna(0).copy()
 liters_pm=liters1_pm.   fillna(0).copy()
 #
-datex=pd.Series(list(liters_am.columns.values ))
-datex.name = 'datex'
+datex       = pd.Series(list(liters_am.columns.values ))
+datex.name  = 'datex'
 #
-maxcols=len(datex)             #1575                          #len of dates (col headers for liters - which starts with 'start_date')
-maxrows= len(wy)   #201                          #len of groupx - will accomodate new calves - continuous series from 1~200 will be the output col heading 
-idx_cols1=[*range(1,maxcols+1)]         #list
+maxcols     = len(datex)             #1575                          #len of dates (col headers for liters - which starts with 'start_date')
+maxrows     = len(wy)   #201                          #len of groupx - will accomodate new calves - continuous series from 1~200 will be the output col heading 
+idx_cols1   = [*range(1,maxcols+1)]         #list
 #
-idx=np.zeros((maxrows,maxcols))                   #creates basic array of zeros
-idx_am=idx.copy()
-idx_pm=idx.copy()
+idx     = np.zeros((maxrows,maxcols))                   #creates basic array of zeros
+idx_am  = idx.copy()
+idx_pm  = idx.copy()
 #
-wy_am1=wy_am.to_numpy(dtype=int)
-wy_pm1=wy_pm.to_numpy(dtype=int)
+# Numpyization
+wy_am1 = wy_am.to_numpy(dtype=int)
+wy_pm1 = wy_pm.to_numpy(dtype=int)
 liters_am1=liters_am.to_numpy(dtype=float)
 liters_pm1=liters_pm.to_numpy(dtype=float)
 
@@ -60,26 +61,26 @@ l = liters_pm1[rows[:, np.newaxis], cols]        #array 70,1813
 idx_pm[w,cols] = l
         
 #
-am2=pd.DataFrame(idx_am)
+am2 = pd.DataFrame(idx_am)
 am2.columns=datex
-am=am2.T
+am = am2.T
 am.replace(0,np.nan,inplace=True)
 am.index=pd.to_datetime(am.index)
 am.drop(am.iloc[:,0:1],axis=1,inplace=True)
 
 #
-pm2=pd.DataFrame(idx_pm)
+pm2 = pd.DataFrame(idx_pm)
 pm2.columns=datex
-pm=pm2.T
+pm = pm2.T
 pm.replace(0,np.nan,inplace=True)
-pm.index=pd.to_datetime(pm.index)
+pm.index = pd.to_datetime(pm.index)
 pm.drop(pm.iloc[:,0:1],axis=1,inplace=True)
 
 #
-fullday1=np.add(idx_am,idx_pm)
-fullday2=pd.DataFrame(fullday1)
-fullday2.columns=datex
-fullday=fullday2.T
+fullday1 = np.add(idx_am,idx_pm)
+fullday2 = pd.DataFrame(fullday1)
+fullday2.columns = datex
+fullday = fullday2.T
 fullday.index.name = 'datex'
 fullday.replace(0,np.nan,inplace=True)
 fullday.index=pd.to_datetime(fullday.index).date
@@ -88,7 +89,7 @@ fullday.index.name = 'datex'
 
 # 10 day
 #
-lastday=fullday.iloc[-1:,:]     #last milking day recorded
+lastday = fullday.iloc[-1:,:]     #last milking day recorded
 ld=lastday.loc[:,(lastday>0).any()].columns.tolist()     #the .any is important
 # ld_nums1 = ld[:] #this drops the final 5 cols (avg, count, year.month,week) which are strings
 ld_nums = [int(x-1) for x in ld] #decrements the wy's
@@ -100,7 +101,7 @@ tenday3 = tenday1.iloc[:,ld_nums]      #unnecessary?
 tendayT=tenday2.T
 # 
 tendayT.columns=[1,2,3,4,5,6,7,8,9,10]
-tenday=tendayT
+tenday = tendayT
 avg = tenday.mean(axis=1).astype(float)
 tendayT['avg']=avg.round(1) 
 # 
@@ -110,15 +111,15 @@ sumx = tenday.sum(axis=0).astype(float)
 # tenday.loc[''] = sumx.round(0)                   # [''] means 'empty row'
 tenday.loc['total'] = tenday.sum(axis=0)
 # 
-allcols=['WY_id','age last calf','i_date','age last insem','u_date','readex','days left']
-all1= all.loc[:,allcols].copy()
-all2 = all1.merge(status['status'],how='left',left_on='WY_id',right_on='WY_id')
+allcols = ['WY_id','age last calf','i_date','age last insem','u_date','readex','days left']
+all1    = all.loc[:,allcols].copy()
+all2    = all1.merge(status['status'],how='left',left_on='WY_id',right_on='WY_id')
  
 
 
 # sum and nonzero count for entire milk df
 #
-milk=fullday
+milk = fullday
 fullday.replace(np.nan,0,inplace=True)
 #
 milkrowcount=   milk.astype(bool).sum(axis=1)
