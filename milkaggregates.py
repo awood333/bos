83d4bf9ce8650407da630e6eea0e045e03d97b7c
alan woodhull <89449956+awood333@users.noreply.github.com>
2023-10-06 06:00:17 +0700

    oct 6

--- milkaggregates.py
+++ milkaggregates.py
@@ -6,87 +6,133 @@ import openpyxl
 # import birthdeath as bd
 # import insem_ultra as iu 
 #
-AM_liters = pd.read_csv     ('F:\\COWS\\data\\milk_data\\raw\\csv\\AM_liters.csv',   index_col=0,header=0)
-AM_wy   = pd.read_csv       ('F:\\COWS\\data\\milk_data\\raw\\csv\\AM_wy.csv',       index_col=0,header=0)
-PM_liters = pd.read_csv     ('F:\\COWS\\data\\milk_data\\raw\\csv\\PM_liters.csv',   index_col=0,header=0)
-PM_wy   = pd.read_csv       ('F:\\COWS\\data\\milk_data\\raw\\csv\\PM_wy.csv',       index_col=0,header=0)
-bd      = pd.read_csv       ('F:\\COWS\\data\\csv_files\\birth_death.csv',                       header=0)
-all     = pd.read_csv       ('F:\\COWS\\data\\insem_data\\all.csv',                              header=0)
-status  = pd.read_csv     ('F:\\COWS\\data\status\\status_column.csv',            index_col=0,header=0)       
+AM_liters = pd.read_csv     ('F:\\COWS\\data\\milk_data\\raw\\csv\\AM_liters.csv',    header=0, dtype=float)
+AM_wy   =   pd.read_csv     ('F:\\COWS\\data\\milk_data\\raw\\csv\\AM_wy.csv',       index_col=0, header=0, dtype=float)
+PM_liters = pd.read_csv     ('F:\\COWS\\data\\milk_data\\raw\\csv\\PM_liters.csv',   index_col=0, header=0)
+PM_wy   = pd.read_csv       ('F:\\COWS\\data\\milk_data\\raw\\csv\\PM_wy.csv',       index_col=0, header=0)
+bd      = pd.read_csv       ('F:\\COWS\\data\\csv_files\\birth_death.csv',                        header=0)
+all     = pd.read_csv       ('F:\\COWS\\data\\insem_data\\all.csv',                               header=0)
+status  = pd.read_csv       ('F:\\COWS\\data\status\\status_column.csv',             index_col=0, header=0)       
 # 
-liters1_am  = AM_liters
-wy1_am      = AM_wy
-liters1_pm  = PM_liters
-wy1_pm      = PM_wy
+lag = -365   # this sets the size of the slice
+
+liters1_am  = AM_liters .iloc[:,lag:]
+wy1_am      = AM_wy     .iloc[:,lag:]
+liters1_pm  = PM_liters .iloc[:,lag:]
+wy1_pm      = PM_wy     .iloc[:,lag:]
 # 
 wy      = bd['WY_id']
 alive1  = bd['death_date'].isnull()
 alive   = wy.loc[alive1].copy()
 alive.reset_index(drop=True,inplace=True)
-#
-wy_am =     wy1_am.       fillna(0).copy()
-wy_pm =     wy1_pm.       fillna(0).copy()
-liters_am = liters1_am.   fillna(0).copy()
-liters_pm = liters1_pm.   fillna(0).copy()
-#
-datex       = pd.Series(list(liters_am.columns.values ))
-datex.name  = 'datex'
-#
+
+wy_am =     wy1_am
+wy_pm =     wy1_pm
+liters_am = liters1_am
+liters_pm = liters1_pm
+
+datex2      = liters_am.T.iloc[:,1:].copy()
+datex2.index.name = 'datex'
+date_format = '%m/%d/%Y'
+datex = pd.to_datetime(datex2.index, format='%m/%d/%Y') #datetime64[ns]
+
+# print(datex2.iloc[:2,:2],'   ',datex[:3])
+
 maxcols     = len(datex)             #1575                          #len of dates (col headers for liters - which starts with 'start_date')
-maxrows     = len(wy)   #201                          #len of groupx - will accomodate new calves - continuous series from 1~200 will be the output col heading 
-idx_cols1   = [*range(1,maxcols+1)]         #list
-#
-idx     = np.zeros((maxrows,maxcols))                   #creates basic array of zeros
+maxrows     = len(bd['WY_id'])   #201                          #len of groupx - will accomodate new calves - continuous series from 1~200 will be the output col heading 
+
+idx     = np.zeros((maxrows+1,maxcols), dtype=int)    
 idx_am  = idx.copy()
 idx_pm  = idx.copy()
-#
+
 # Numpyzation
-wy_am1 =    wy_am.      to_numpy(dtype=int)
-wy_pm1 =    wy_pm.      to_numpy(dtype=int)
+wy_am1 =    wy_am.      to_numpy(dtype=float)
+wy_pm1 =    wy_pm.      to_numpy(dtype=float)
 liters_am1= liters_am.  to_numpy(dtype=float)
 liters_pm1= liters_pm.  to_numpy(dtype=float)
 
-
-
 #   AM calc
-#
-rows = np.array(range(0, 70))  
-cols = np.array(range(0, len(datex)))  # Convert to NumPy array
-# 
-w = wy_am1[rows    [:, np.newaxis],    cols]     #array 70,1813
-l = liters_am1[rows[:, np.newaxis], cols]        #array 70,1813
-idx_am[w,cols] = l
-# 
+
+# static example -- don't erase
+# index1 = wy_am1[:,1]    #70,1869
+# index2 = index1[~np.isnan(index1)].astype(int)
+
+# value1 = liters_am1[:,1]   #70 1869
+# value2 = value1[~np.isnan(value1)].astype(float)
+# target1 = idx_am[:,1]      #243 1869
+# target1[index2] = value2
+
+#  loop
+
+idx_cols1   = [*range(1,maxcols)]         #list
+target_am = []
+i = 0
+
+while i < maxcols:
+    index1 = wy_am1[:,i]    #70,1869
+    index2 = np.nan_to_num(index1, nan=0).astype(int)
+
+    value1 = liters_am1[:,i]   #70 1869
+    value2 = np.nan_to_num(value1, nan=0.0).astype(float)
+    target1 = idx_am[:,i].astype(float)      #243 1869
+
+    target1[index2] = value2
+    target_am.append(target1)
+    i += 1
+am1 = pd.DataFrame(target_am)
+
 #   PM calc
 #
-w = wy_pm1[rows    [:, np.newaxis],    cols]     #array 70,1813
-l = liters_pm1[rows[:, np.newaxis], cols]        #array 70,1813
-idx_pm[w,cols] = l
-        
+
+idx_cols1   = [*range(1,maxcols)]         #list
+target_pm = []
+i = 0
+
+while i < maxcols:
+    index1 = wy_pm1[:,i]    #70,1869
+    index2 = np.nan_to_num(index1, nan=0).astype(int)
+
+    value1 = liters_pm1[:,i]   #70 1869
+    value2 = np.nan_to_num(value1, nan=0.0).astype(float)
+    target1 = idx_pm[:,i].astype(float)      #243 1869
+
+    target1[index2] = value2
+    target_pm.append(target1)
+    i += 1
+pm1 = pd.DataFrame(target_pm)
+
+
+
+
+
+
+
 #
-am2 = pd.DataFrame(idx_am)
-am2.columns=datex
+am2 = pd.DataFrame(am1)
 am = am2.T
+am.columns=datex
 am.replace(0,np.nan,inplace=True)
-am.index=pd.to_datetime(am.index)
+# am.index=pd.to_datetime(am.index)
 am.drop(am.iloc[:,0:1],axis=1,inplace=True)
 
 #
-pm2 = pd.DataFrame(idx_pm)
-pm2.columns=datex
+pm2 = pd.DataFrame(pm1)
 pm = pm2.T
+pm.columns=datex
 pm.replace(0,np.nan,inplace=True)
-pm.index = pd.to_datetime(pm.index)
+# pm.index = pd.to_datetime(pm.index)
 pm.drop(pm.iloc[:,0:1],axis=1,inplace=True)
 
 #
-fullday1 = np.add(idx_am,idx_pm)
+fullday1 = np.add(am1,pm1)  #cols are wy's, index is days
 fullday2 = pd.DataFrame(fullday1)
-fullday2.columns = datex
-fullday = fullday2.T
-fullday.index.name = 'datex'
-fullday.replace(0,np.nan,inplace=True)
+fullday2['datex'] = datex
+fullday2.set_index('datex', inplace=True)
+
+fullday = fullday2
 fullday.index=pd.to_datetime(fullday.index).date
+fullday.replace(0,np.nan,inplace=True)
+
 fullday.drop(fullday.iloc[:,0:1],axis=1,inplace=True)
 fullday.index.name = 'datex'
 
@@ -94,7 +140,6 @@ fullday.index.name = 'datex'
 #
 lastday = fullday.iloc[-1:,:]     #last milking day recorded
 ld=lastday.loc[:,(lastday>0).any()].columns.tolist()     #the .any is important
-# ld_nums1 = ld[:] #this drops the final 5 cols (avg, count, year.month,week) which are strings
 ld_nums = [int(x-1) for x in ld] #decrements the wy's
 # 
 tenday1 = fullday.iloc[-10:,:].copy() # has all wy's
@@ -117,12 +162,10 @@ tenday.loc['total'] = tenday.sum(axis=0)
 allcols = ['WY_id','age last calf','i_date','age last insem','u_date','readex','days left']
 all1    = all.loc[:,allcols].copy()
 all2    = all1.merge(status,how='left',left_on='WY_id',right_on='WY_id')
- 
-# all2    = all1.merge(status['status'],how='left',left_on='WY_id',right_on='WY_id')
 
 # sum and nonzero count for entire milk df
 #
-milk = fullday
+milk = fullday.copy()
 fullday.replace(np.nan,0,inplace=True)
 #
 milkrowcount =   milk.astype(bool).sum(axis=1)
@@ -141,9 +184,10 @@ milk['year']=milk.index.year
 milk['month']=milk.index.month
 milk['week']=milk.index.isocalendar().week
 #  the as_index=False leaves the new columns accessible for .loc, otherwise they become part of a multi-index
-milk_monthly_sum   =   milk.groupby(['year','month'],          as_index=False).sum()    
+milk_monthly_sum    =   milk.groupby(['year','month'],          as_index=False).sum()    
 milk_monthly_mean1  =   milk.groupby(['year','month'],          as_index=False).mean()     
-weekly         =   milk.groupby(['year','month','week'],   as_index=False).mean() 
+weekly              =   milk.groupby(['year','month','week'],   as_index=False).mean() 
+
 # 
 # 
 # change names because 'sum' will eventually mean the monthly total vs the avg
@@ -156,20 +200,20 @@ monthly1['total'] = milk_monthly_sum['avg']
 def format_num(num):
     return '{:,.0f}'.format(num)
 # 
-monthly1[['avg count', 'avg sum', 'total']] = monthly1[['avg count', 'avg sum', 'total']].applymap(format_num)
+monthly1[['avg count', 'avg sum', 'total']] = monthly1[['avg count', 'avg sum', 'total']].map(format_num)
 monthly = monthly1.reset_index(drop=True)
 
 
 
 # WRITE TO CSV
-#
-am.to_csv('F:\\COWS\data\\milk_data\\halfday\\am\\am.csv')
+
+am.to_csv('F:\\COWS\data\\milk_data\\halfday\\am\\am.csv')  #these are useful to check against daily_milk to see if the data is aligned
 pm.to_csv('F:\\COWS\data\\milk_data\\halfday\\pm\\pm.csv')
 # 
 fullday_lastdate = pd.DataFrame(index=[fullday.index[-1]], columns=['last_date'])
 
 # 
-fullday.to_csv('F:\\COWS\data\\milk_data\\fullday\\fullday.csv')
+fullday.        to_csv('F:\\COWS\data\\milk_data\\fullday\\fullday.csv')
 fullday_lastdate.to_csv('F:\\COWS\data\\milk_data\\fullday\\fullday_lastdate.csv')
 # 
 weekly.         to_csv('F:\\COWS\data\\milk_data\\totals\\weekly.csv')
--- status.py
+++ status.py
@@ -1,118 +1,109 @@
+'''
+status.py
+'''
 import pandas as pd
 import numpy as np
-import datetime as dt
-from datetime import date
-#
-f1 = pd.read_csv('F:\\COWS\\data\\milk_data\\fullday\\fullday.csv')
-bd=pd.read_csv('F:\\COWS\\data\\csv_files\\birth_death.csv',index_col=0,header=0,parse_dates=['birth_date','death_date'],low_memory=False,dtype=None)
+
+f  = pd.read_csv('F:\\COWS\\data\\milk_data\\fullday\\fullday.csv')
+f.set_index('datex', inplace=True)
+f.index = pd.to_datetime(f.index)
+bd  = pd.read_csv('F:\\COWS\\data\\csv_files\\birth_death.csv',index_col=0,header=0,parse_dates=['birth_date','death_date','arrived', 'adj_bdate'],low_memory=False,dtype=None)
+
+# def adj_bdate(bd):
+#     bd['adj_bdate'] = bd.apply(
+#         lambda row: row['birth_date'] 
+#         if pd.isna(row['arrived'])
+#         else(row['arrived']) 
+#         , axis=1
+#         )
+#     return bd
+
 date_names = ['age cow','stop_last','lastcalf bdate','i_date','u_date','next bdate','ultra(e)']
-# 
-# NOTE: 'status' reads from 'all' so don't make circular references
-# 
-everything=pd.read_csv('F:\\COWS\\data\\insem_data\\all.csv',index_col=0,header=0,parse_dates=date_names, date_format='%m/%d/%Y',low_memory=False,dtype=None)
-#
-f = f1.iloc[:,:-5].copy()
-bdidx=      bd.index.to_series()
-idx=        np.arange(1,len(bdidx)+1)
-idxforloop= np.arange(0,len(bdidx))
-#
-status=     pd.DataFrame()
-status['WY_id']=idx
-status.set_index('WY_id',inplace=True)
-#
-bdmax=bd.index.max()+2
-rng=list(range(1,bdmax))         #blank df with WYs as index
-lastliveheifer=bdmax
-lastdate=f.iloc[-1:,0].to_string(index=False)
-df= pd.DataFrame(columns=['status']) 
-df['WY_id']=rng
-df.set_index('WY_id',drop=True,inplace=True)
+everything = pd.read_csv('F:\\COWS\\data\\insem_data\\all.csv',index_col=0,header=0,parse_dates=date_names, date_format='%m/%d/%Y',low_memory=False,dtype=None)
 
-# Alive / dead
-bd2=bd.iloc[:lastliveheifer,:].copy()
-alive=bd2.loc[
-    (bd2['death_date'].isnull()
-    )    ].copy()
-alive['alive']='alive'
-alivelist=list(alive.index.values)
-alivelist.sort()
-alivex=pd.DataFrame(alivelist,columns=['alive'])
-#
-dead=bd.loc[(bd['death_date'].notnull())    ].copy()
-dead['dead']='dead'
-deadlist=dead.index.tolist()
-#
-alive_count=len(alivelist)
-dead_count=len(deadlist)
-df.loc[deadlist] ='gone'
+# NOTE: 'status' reads from 'all' so don't make circul ar references
 
-# Heifers
-#
-heifers = everything.loc[(
-        everything['death_date'].isnull()
-    &   everything['stop_last'].isnull()
-    &   everything['lastcalf bdate'].isnull()
-    )].copy()
-# 
-heiferlist=heifers.index.tolist()
-heiferlist.sort()
-heiferx=pd.DataFrame(heiferlist,columns=['heifer'])
-heifer_count=len(heiferlist)
-df.loc[heiferlist]= 'H'
+bdmax       = bd.index.max()                    # last cow WY_id
+idx3        = range(1, bdmax + 1)
+lastrow     = f.iloc[-1:,0].to_string(index=False)  # last row of milking values - floats
 
-# Milking cows on last milking day on record
-#
-milking=f.T.iloc[1:,-1] .copy()   
-milking.index.name='WY_id'
-milkinglistx=[]
-for i in milking.index:
-    if milking[i] >0:
-        milkinglistx.append(i)
-    elif milking[i]==0:
-        pass
-milkinglist=[int(x) for x in milkinglistx]
-milkinglist.sort()
-milking_count=len(milkinglist)
-df.loc[milkinglist]='M'
-milkx=pd.DataFrame(milkinglist,columns=['milking'])
+maxdate     = f.index.max()                  #timestamp of datex -  the index
+mindate     = f.index.min()
+startdate   = '2023-7-24'
+stopdate    = '2023-9-24'
 
-# Dry
-#
-notdry= milkinglist + heiferlist
-dry1=set(alivelist).difference(set(notdry))    #set diff
-drylist=list(dry1)                                  # sets have a problem
-drylist.sort()
-dryx=pd.DataFrame(drylist)
-dryx.rename(columns={0:'dry'},inplace=True)
-dry_count=len(drylist)
-df.loc[drylist]='dry'
+daterange   = pd.date_range(startdate, stopdate, freq='D') 
 
-# Comparison to find duplicates
-#
-notdry= milkinglist + heiferlist
-set_sym=set(alivelist).symmetric_difference(drylist)              #gets elements NOT in common
-set_diff=set(alivelist).difference(set(notdry))
-set_intersection=set(milkinglist)&set(heiferlist)    #set intersection
-#
-allx=[*milkinglist,*drylist,*heiferlist]
-set_diff2=set(alivelist).difference(set(allx))
+def alive(mask):
+    mask =  np.zeros((len(daterange), bdmax), dtype=bool)
+    for i, date_i in enumerate(daterange):
+         
+        for j in range(1, bdmax):  
+             
+            var1 = bd['adj_bdate'][j]
+            var2 = bd['death_date'][j]
+            
+            condition = (
+                (var1 <= date_i) &
+                ((var2 > date_i) | (pd.isnull(var2) == True))
+            )
+            mask[i, j-1] = condition
+  
 
-# Status df
-collist=[alivex,milkx,dryx,heiferx]
-sl1=pd.merge(left=alivex,right=milkx,how='outer',left_index=True,right_index=True)
-sl2=pd.merge(left=sl1,right=dryx,   how='outer',left_index=True,right_index=True)
-sl3=pd.merge(left=sl2,right=heiferx,how='outer',left_index=True,right_index=True)
+    alive_mask = pd.DataFrame(mask, columns=idx3, index=daterange)
+    return alive_mask
 
-# Box score
-allcows_count=milking_count+dry_count+heifer_count
-cols2=['date','alive_count','milking_count','dry_count','heifer_count','dead_count','total']
-boxscore=pd.DataFrame(index=None,columns=['type','count'])
-boxscore['type']=['date','milking_count','dry_count','heifer_count','alive_count','total']
-boxscore['count'] = [lastdate,milking_count,dry_count,heifer_count,alive_count,allcows_count]
+alive_mask = pd.DataFrame()     #next line wants a blank df
+alive_mask = alive(alive_mask)
 
-#  write to csv
-sl3.to_csv('F:\\COWS\\data\\status\\status_lists.csv')
-df.to_csv('F:\\COWS\\data\\status\\status_column.csv')
-boxscore.to_csv('F:\\COWS\\data\\status\\boxscore.csv')
+   
+def  alive_count(alive_mask):
+    # counts = []
+    # for col in  alive_mask.columns:
+    #     col_sum = np.sum(alive_mask[col] == True)
+    #     counts.append(col_sum)
+    # alive_count = pd.Series(counts, index=alive_mask.columns)
+    return alive_mask.sum()
+
+alive_count_df    = pd.DataFrame(alive_count(alive_mask.T))
+
+def alive_ids(booldf, bval):
+    alive_ids = booldf.apply(lambda col: np.where(col, col.index, ''), axis=0)
+    return alive_ids
+  
+alive_ids_df       = pd.DataFrame(alive_ids(alive_mask.T, True))
+
+
+f1 = f.loc[daterange].copy()
+
+def milkers():
+    mask = np.zeros((len(daterange), bdmax), dtype=bool)
+    mask1, mask2, milkers_mask = [], [], []
+
+    for i, date_i in enumerate(daterange, 0):
+        for j in range(1, bdmax):
+            mask1 = f1.iloc[i,j] > 0 
+            mask2.append(mask1)
+        milkers_mask.append(mask2)
+        mask2 = []
+    return milkers_mask
+    
+
+def milkers_ids(milkers_mask):
+    milkers_ids = np.where(milkers_mask, np.arange(1,bdmax), np.nan)
+    return milkers_ids
+
+milkers_mask = milkers()
+milkers_ids_df = pd.DataFrame(milkers_ids(milkers_mask))
+
+
+alive_count_df .to_csv('F:\\COWS\\data\\status\\alive_count.csv')
+alive_ids_df   .to_csv('F:\\COWS\\data\\status\\alive_ids.csv')
+milkers_ids_df .to_csv('F:\\COWS\\data\\status\\milkers_ids.csv')
+
+# #  write to csv
+# sl3.to_csv('F:\\COWS\\data\\status\\status_lists.csv')
+# df.to_csv('F:\\COWS\\data\\status\\status_column.csv')
+# boxscore.to_csv('F:\\COWS\\data\\status\\boxscore.csv')
 
 
