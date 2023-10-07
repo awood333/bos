d78f6046bfeb196baeae976f62487ae7d79ab566
alan woodhull <89449956+awood333@users.noreply.github.com>
2023-10-07 10:10:10 +0700

    10/7 10:00

--- status.py
+++ status.py
@@ -3,10 +3,7 @@ status.py
 '''
 import pandas as pd
 import numpy as np
-import time
-
-
-
+# import time
 
 f  = pd.read_csv('F:\\COWS\\data\\milk_data\\fullday\\fullday.csv')
 f.set_index('datex', inplace=True)
@@ -16,8 +13,6 @@ bd  = pd.read_csv('F:\\COWS\\data\\csv_files\\birth_death.csv',
     header=0,
     parse_dates=['birth_date','death_date','arrived', 'adj_bdate'],
     low_memory=False,dtype=None)
-
-# bd['adj_bdate'] = bd.apply(lambda row: row['birth_date'] if pd.isna(row['arrived']) else row['arrived'], axis=1)
 
 date_names = ['age cow','stop_last','lastcalf bdate','i_date','u_date','next bdate','ultra(e)']
 everything = pd.read_csv('F:\\COWS\\data\\insem_data\\all.csv',
@@ -58,43 +53,45 @@ def create_alive_mask(bd, date_range):
     alive_mask = pd.DataFrame(mask, columns=idx3, index=date_range)
     return alive_mask
 
-alive_mask = create_alive_mask(bd, date_range)
-alive_count_df = alive_mask.sum().to_frame()
+alive_mask = create_alive_mask(bd, date_range)          #calls the function 
+alive_count_df = alive_mask.sum(axis=1).to_frame()            # calc 'count' and converts to df
 
-def create_alive_ids(alive_mask, date_range_cols):
-    result = alive_mask.copy()  # Create a copy of alive_mask to store the results
-    
+
+def create_alive_ids(alive_mask, date_range_cols):      #get col headers (WY_ids) for each live cow
+    result = alive_mask.copy()                          
     for col in alive_mask.columns:
         result[col] = np.where(alive_mask[col], col, np.nan)
     
     alive_ids_df = pd.DataFrame(result, columns=alive_mask.columns, index=date_range_cols)
     return alive_ids_df
 
+alive_ids_df = create_alive_ids(alive_mask, date_range_cols)    #call the function
 
-alive_ids_df = create_alive_ids(alive_mask, date_range_cols)
 
-f1 = f.loc[date_range].copy()
 
-milkers_mask = f1>0
+f1 = f.loc[date_range].copy()    #partition the milk dbase
 
-def create_milkers_ids(milkers_mask, date_range_cols):
+milkers_mask = f1>0         # create mask of all milking activity - col headers are WY_id
+
+def create_milkers_ids(milkers_mask, date_range_cols):      # WY_id headers
     result = milkers_mask.copy()
     for col in milkers_mask.columns:
-        result[col] = np.where(milkers_mask[col], col, np.nan)
+        result[col] = np.where(milkers_mask[col], col, np.nan)      #returns the col header where True, or np.nan for the False
     
     milkers_ids_df = pd.DataFrame(result, columns=milkers_mask.columns, index=date_range_cols)
     return milkers_ids_df
 
-milkers_ids_df = create_milkers_ids(milkers_mask, date_range_cols)
-milkers_count_df = milkers_mask.sum(axis=1).to_frame()
-
+milkers_ids_df = create_milkers_ids(milkers_mask, date_range_cols)  #all cows, all dates, with col headers (WY_id) where True
+milkers_count_df = milkers_mask.sum(axis=1).to_frame()              #count of True in each row (date)
 
 alive_masknp = alive_mask.to_numpy()
 milkers_masknp = milkers_mask.to_numpy()
-dry_mask = alive_masknp ^ milkers_masknp
 
 
 
+# dry_mask = alive_masknp ^ milkers_masknp    #numpy notation for -
+
+dry_mask = np.logical_and(alive_masknp, np.logical_not(milkers_mask))
 
 def create_dry_ids(dry_mask, date_range_cols):
     result = dry_mask.copy()
@@ -108,13 +105,14 @@ dry_ids_df   = create_dry_ids(dry_mask, date_range_cols)
 dry_count_df = dry_mask.sum(axis=1).to_frame() 
 
 # write to csv
-alive_count_df  .to_csv('F:\\COWS\\data\\status\\alive_count.csv', index=False)
-alive_ids_df    .to_csv('F:\\COWS\\data\\status\\alive_ids.csv', index=False)
+alive_count_df  .to_csv('F:\\COWS\\data\\status\\alive_count.csv')
+alive_ids_df    .to_csv('F:\\COWS\\data\\status\\alive_ids.csv')
+
 milkers_ids_df  .to_csv('F:\\COWS\\data\\status\\milkers_ids.csv')
 milkers_count_df.to_csv('F:\\COWS\\data\\status\\milkers_count.csv')
+
 dry_ids_df      .to_csv('F:\\COWS\\data\\status\\dry_ids.csv')
 dry_count_df    .to_csv(('F:\\COWS\\data\\status\\dry_count.csv'))
 
-dry_count_df    .to_csv(('F:\\COWS\\data\\status\\alive1df.csv'))
-milkers1df    .to_csv(('F:\\COWS\\data\\status\\milkers1df.csv'))
+
 
