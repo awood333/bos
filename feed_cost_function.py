9c9005f061826c38713207ddccb79e4203f6df80
alan woodhull <89449956+awood333@users.noreply.github.com>
2023-10-14 16:05:26 +0700

    feed_cost_function 10/14 1605

--- feed_cost_function.py
+++ feed_cost_function.py
@@ -1,64 +1,81 @@
 '''
+feed_cost_function.py
 '''
 import pandas as pd
 import numpy as np
 import datetime
-import status 
 
 
+startdate   = '4/1/2023'
+stopdate    = '10/1/2023'
+date_range = pd.date_range(startdate, stopdate)
 
-def create_feed_data_timeline(price, sequence,bunker_lot, weight, event):
-    corn        = pd.read_csv('F:\\COWS\\data\\csv_files\\feed_csv\\corn.csv',      header=0, index_col=None, parse_dates=['datex'])
-    cassava     = pd.read_csv('F:\\COWS\\data\\csv_files\\feed_csv\\cassava.csv',       header=0, index_col=None, parse_dates=['datex'])
-    beans       = pd.read_csv('F:\\COWS\\data\\csv_files\\feed_csv\\yellow_beans.csv',  header=0, index_col=None, parse_dates=['datex'])
+
+def create_feed_data_timeline():
     
-    amt_cassava = pd.read_csv('F:\\COWS\\data\\csv_files\\feed_csv\\cassava_amount.csv',   header=0, index_col=None, parse_dates=['datex'])
-    amt_yellow_bean = pd.read_csv('F:\\COWS\\data\\csv_files\\feed_csv\\yellow_bean_amount.csv',   header=0, index_col=None, parse_dates=['datex'])
+    corn        = pd.read_csv('F:\\COWS\\data\\csv_files\\feed_csv\\corn.csv',
+        header=0, index_col=0, parse_dates=['datex','open_date', 'end_date'])
+    status1      =  pd.read_csv('F:\\COWS\\data\\status\\status_all.csv',
+        header=0, index_col='datex', parse_dates=['datex'])
 
-    feed = corn
+    status = status1.loc[date_range]        
+ 
+    result = corn.reindex(date_range, method='ffill')
+    result.index.name = 'datex'
+    
+    
 
-    startdate   = '7/1/2023'
-    stopdate    = '10/1/2023'
+    # duration column
+    duration = (result['end_date'] - result['open_date']).dt.days
+    result['duration'] = duration
 
-    date_range = status.create_date_range(startdate, stopdate)
+
+    corn_df = pd.DataFrame(result)
+    
+    cc = corn_df.merge(status, left_index=True, right_index=True)
+   
+    cowdays3 = []
+    avg_daily_amt1 = []
+
+    for i in corn.sequence:
+
+        bunkstart = corn['open_date'][i-1]
+        bunkend     = corn['end_date'][i-1]
+        bunk_range = pd.date_range(bunkstart, bunkend, freq='D')
+        bunk_range2 = cc.loc[bunk_range].copy()   
+        cowdays1 = bunk_range2['alive'].sum()
+        cowdays2 = pd.Series(cowdays1, index=bunk_range)
+        avg_daily_amt2 = (bunk_range2['weight'] / cowdays1)
+     
+        cowdays3.append(cowdays2)
+        
+        avg_daily_amt1.append(avg_daily_amt2)
 
 
-    # lop = None  # Last Observed Price
-    price       = pd.DataFrame()
-    sequence    = pd.DataFrame()
-    bunker_lot  = pd.DataFrame()
-    weight      = pd.DataFrame()
-    event       = pd.DataFrame()
+    avg_daily_amt   = [item for sublist in avg_daily_amt1 for item in sublist]       #flattens the list from the loop
+    cowdays        = [item for sublist in cowdays3       for item in sublist]       #flattens the list from the loop          
+     
+        
+    groupa_consumption  = cc['group_a'] * avg_daily_amt
+    groupb_consumption  = cc['group_b'] * avg_daily_amt
+    dry_consumption     = cc['dry'] * avg_daily_amt    
+    daily_consumption   = (groupa_consumption + groupb_consumption + dry_consumption)
+    daily_corn_cost     = daily_consumption * cc['unit_price']
+    
+    
+    cc['cowdays']       = cowdays
+    cc['avg_daily_amt'] = avg_daily_amt
+    cc['groupa consum'] = groupa_consumption
+    cc['groupb consum'] = groupb_consumption
+    cc['dry consum']    = dry_consumption
+    cc['daily total']   = daily_consumption
+    cc['daily corn cost'] = daily_corn_cost
+    
 
-    timeline    = pd.DataFrame(index=date_range)
+    return corn_df, cc
 
-    for index, row in feed.iterrows():
-        if not pd.isna(row['price']):
-            lop             = row['price']             #Last Observed Price
-            last_sequence   = row['sequence']
-            last_bunker     = row['bunker_lot']
-            last_weight     = row['weight']
+corn_df, cc = create_feed_data_timeline()
 
-        if last_sequence is not None:
-            start_date  = row['datex']
-            stop_date   = corn.iloc[index + 1]['datex'] if index + 1 < len(corn) else stopdate
-            date_range_chunk = pd.date_range(start=start_date, end=stop_date, freq='D')
+cc.to_csv('F:\\COWS\\data\\feed_data\\feed_corn.csv')
+x=1
 
-            price = pd.concat([price, pd.Series([lop] * len(date_range_chunk), index=date_range_chunk)])
-            sequence = pd.concat([sequence, pd.Series([last_sequence] * len(date_range_chunk), index=date_range_chunk)])
-            bunker_lot = pd.concat([bunker_lot, pd.Series([last_bunker] * len(date_range_chunk), index=date_range_chunk)])
-            weight = pd.concat([weight, pd.Series([last_weight] * len(date_range_chunk), index=date_range_chunk)])
-
-    for index, row in corn.iterrows():
-        if row['event']     == 'start':
-            start_date      =   row['datex']
-        elif row['event']   == 'stop':
-            stop_date       =   row['datex']
-            timeline.loc[start_date:stop_date, event] = row[event]
-
-    timeline = pd.concat([sequence,bunker_lot, price, weight, event], axis=1)
-
-    return timeline
-
-feed_data_timeline = create_feed_data_timeline('sequence','bunker_lot', 'price', 'weight', 'event')
-print(feed_data_timeline)
