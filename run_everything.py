871aa61428c27570eb64ead9e1b3d4e7cf4aa3d4
alan woodhull <89449956+awood333@users.noreply.github.com>
2023-10-24 06:05:20 +0700

    10/24 0600

--- run_everything.py
+++ run_everything.py
@@ -1,10 +1,7 @@
 import subprocess
 import pandas as pd
 from datetime import datetime
-# import sys
-# print("sys exec  ",sys.executable)
-# print("sys path",sys.path)
-# 
+from status import main
 # 
 dmAM_liters=pd.read_excel ('F:\\COWS\\data\\daily_milk.xlsm',     sheet_name='AM_liters', skiprows=2,index_col=0,header=0)
 AM_liters=pd.read_csv   ('F:\\COWS\\data\\milk_data\\raw\\csv\\AM_liters.csv',          index_col=0,header=0)
@@ -20,9 +17,9 @@ d2 = AM_liters.columns[-1]
 
 #  ['rawmilkupdate.py', 'milkaggregates.py']
 script_names = [
-                'D:/Git_repos/bos/insem_ultra.py',
-                'D:/Git_repos/bos/rawmilkupdate.py',
-                'D:/Git_repos/bos/milkaggregates.py',              
+                # 'D:/Git_repos/bos/insem_ultra.py',
+                # 'D:/Git_repos/bos/rawmilkupdate.py',
+                # 'D:/Git_repos/bos/milkaggregates.py',              
                     # 'D:/Git_repos/bos/status.py',
                     # 'D:/Git_repos/bos/status2.py',
                     # 'D:/Git_repos/bos/wet_dry.py'              
@@ -31,3 +28,21 @@ script_names = [
 
 for script in script_names:
     subprocess.run(['python', script])
+    
+    
+    
+# #   from status, 
+
+
+startdate = '2023-6-1'
+maxdate = dmt2.max()
+stopdate    = maxdate
+date_format = '%Y-%m-%d'
+
+sd = main(startdate, stopdate, date_format)
+
+sd['allcows']        .to_csv('F:\\COWS\\data\\status\\status_all.csv')
+sd['dry_ids_df']     .to_csv('F:\\COWS\\data\\status\\dry_ids_df.csv')
+sd['dry_count_df']   .to_csv('F:\\COWS\\data\\status\\dry_ids_df.csv')
+sd['milkers_ids']    .to_csv('F:\\COWS\\data\\status\\milkers_ids_df.csv')    
+sd['days_milking_df'].to_csv('F:\\COWS\\data\\status\\days_milking_df.csv')
