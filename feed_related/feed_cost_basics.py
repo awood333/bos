1972df093141890852cd9de7c1c909cf8b130ecf
alan woodhull <89449956+awood333@users.noreply.github.com>
2024-09-24 05:43:39 +0700

    9/24

--- feed_related/feed_cost_basics.py
+++ feed_related/feed_cost_basics.py
@@ -1,38 +1,85 @@
 '''feed_cost_basics.py'''
 
+import os
+from datetime import datetime
 import pandas as pd
+
+from CreateStartDate import DateRange 
 
 class FeedCostBasics:
     def __init__(self):
-        startdate = pd.to_datetime('6/1/2023')
-        enddate     = pd.to_datetime('9/1/2024')
-        self.rng = pd.date_range(startdate, enddate, freq='D')
+        self.rng = DateRange().date_range
+        self.feed_type =  ['corn','cassava','beans','straw'] 
+
+        self.price_seq_dict, self.daily_amt_dict = self.create_feed_cost()
         
-        self.corn_price_seq = self.create_corn_cost_series()
-        self.cassava_price_seq = self.create_cassava_cost_series()
+        [
+        self.corn_psd,
+        self.cassava_psd,
+        self.beans_psd,
+        self.straw_psd, 
         
-    def create_corn_cost_series(self):
-        cps = pd.read_csv('F:\\COWS\\data\\feed_data\\feed_csv\\corn_price_seq.csv', header=0)
-        cps1 = cps.copy()
-        cps1['datex'] = pd.to_datetime(cps1['datex'])
-        cps1.set_index(['datex'], inplace=True)
+        self.corn_dad,
+        self.cassava_dad,
+        self.beans_dad,
+        self.straw_dad
+        ]                       = self.create_separate_feed_series()
+            
+
+    def create_feed_cost(self):
         
-        self.corn_price_seq = cps1.reindex(self.rng, method='ffill').bfill()
-        return self.corn_price_seq    
-         
-         
-    def create_cassava_cost_series(self):
-        cassava_price_seq = pd.read_csv('F:\\COWS\\data\\feed_data\\feed_csv\\cassava_price_seq.csv', header=0)
-        cps1 = cassava_price_seq.copy()
-        cps1['datex'] = pd.to_datetime(cps1['datex'])
-        cps1.set_index(['datex'], inplace=True)
+            
+        base_path = 'F:/COWS/data/feed_data/feed_csv'
+        self.price_seq_dict = {}
+        self.daily_amt_dict = {}
         
-        self.cps = cps1.reindex(self.rng, method='ffill').bfill()
-        return self.cps
+        for feed in self.feed_type:        
+            price_seq_path = os.path.join(base_path, f'{feed}_price_seq.csv')
+            daily_amt_path = os.path.join(base_path, f'{feed}_daily_amt.csv')
+
+            price_seq   = pd.read_csv(price_seq_path,  header=0, index_col=0)  
+            daily_amt   = pd.read_csv(daily_amt_path,  header=0, index_col=0)  
         
+            price_seq.index = pd.to_datetime(price_seq.index)                 
+            price_seq = price_seq.reindex(self.rng, method='ffill')
+
+
+            daily_amt.index = pd.to_datetime(daily_amt.index)                             
+            daily_amt = daily_amt.reindex(self.rng, method='ffill').bfill()
+            
+            self.price_seq_dict[feed] = price_seq
+            self.daily_amt_dict[feed] = daily_amt
+            
+        return self.price_seq_dict, self.daily_amt_dict
+    
+    def create_separate_feed_series (self):
+        psd = self.price_seq_dict
+        dad = self.price_seq_dict
+        self.corn_psd       = psd['corn']
+        self.cassava_psd    = psd['cassava']
+        self.beans_psd      = psd['beans']
+        self.straw_psd      = psd['straw'] 
         
+        self.corn_dad       = dad['corn']
+        self.cassava_dad    = dad['cassava']
+        self.beans_dad      = dad['beans']
+        self.straw_dad      = dad['straw']
+        
+        return  [
+        self.corn_psd,
+        self.cassava_psd,
+        self.beans_psd,
+        self.straw_psd, 
+        
+        self.corn_dad,
+        self.cassava_dad,
+        self.beans_dad,
+        self.straw_dad
+        ]
+        
+       
         
 if __name__ == "__main__":
     FBB = FeedCostBasics()
-    FBB.create_cassava_cost_series()
+    
                  
