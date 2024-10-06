d0c031ff5a99cd1d9ee710f78bcc2d9e0d92928c
alan woodhull <89449956+awood333@users.noreply.github.com>
2024-10-07 05:18:48 +0700

    10/7

--- milk_functions/days_of_milking.py
+++ milk_functions/days_of_milking.py
@@ -23,111 +23,93 @@ class DaysOfMilking:
         self.date_range_monthly = date_range.create_date_range_monthly()
 
         
-        self.df_results             = self.processDaysOfMilking()
-        self.result_df2             = self.fix_col_headers()
-        self.countA, self.countB    = self.create_group_dfs()
+        self.processDaysOfMilking()
+        # self.result_df2             = self.fix_col_headers()
+        # self.countA, self.countB    = self.create_group_dfs()
         
-        [self.countA_monthly, 
-            self.countB_monthly, 
-            self.all_monthly  ]  = self.create_monthly()
+        # [self.countA_monthly, 
+        #     self.countB_monthly, 
+        #     self.all_monthly  ]  = self.create_monthly()
         
-        self.days_of_milking_dash_vars  = self.get_dash_vars()
-        self.write_to_csv()
+        # self.days_of_milking_dash_vars  = self.get_dash_vars()
+        # self.write_to_csv()
 
 
     def processDaysOfMilking(self):
         date_range = pd.date_range(start='2016-09-01', end=self.enddate_daily, freq='D')
-        count = pd.DataFrame(index=date_range)
-        series_list = []
-
-        for i, sublist in enumerate(self.DOM[0]):
-            concatenated_series = None
-            for j, series in enumerate(sublist):
-                if not series.empty:
-                    if concatenated_series is None:
-                        concatenated_series = series
-                    else:
-                        concatenated_series = pd.concat([concatenated_series, series])
-                        
-            if concatenated_series is not None: 
-                concatenated_series = concatenated_series.sort_index()
-                concatenated_series = concatenated_series.reindex(date_range, fill_value=0)
-                concatenated_series.name = f'list_{i}'
-                series_list.append(concatenated_series)
-
-        count = pd.concat([count] + series_list, axis=1)
+        rng = pd.date_range(start='2016-09-01', end='2016-09-02')
+        series2 = pd.DataFrame(index=rng)
+        series3 = pd.DataFrame(index=rng)
+        series4 = pd.DataFrame(index=rng)
+        series5=[]  
         
-        # Group series by their first numeral
-        grouped_series = {}
-        for col in count.columns:
-            if col.startswith('list_'):
-                group = col.split('_')[1]
-                if group not in grouped_series:
-                    grouped_series[group] = []
-                grouped_series[group].append(count[col])
+        dom = self.DOM
+        
+        days1 = dom[0]
+        # print(days1)
+        
+        
+        return
 
-        # Concatenate grouped series and add to a new DataFrame
-        concatenated_series_dict = {}
-        for group, series_list in grouped_series.items():
-            if len(series_list) == 1:  # Check if there's only one series
-                concatenated_series = series_list[0]
-            else:
-                concatenated_series = pd.concat(series_list, axis=0)
-            concatenated_series = concatenated_series.sort_index()
-            concatenated_series = concatenated_series.reindex(date_range, fill_value=0)
-            concatenated_series_dict[f'list_{group}'] = concatenated_series
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def fix_col_headers(self):
+    #     df1 = self.result_df
+    #     dfcols = self.result_df.columns
+    #     newheaders=[]
+    #     for i in dfcols:
+    #         header =  i.split('_',1)[1]
+    #         newheaders.append(header)
             
-            result_df1 = pd.DataFrame(concatenated_series_dict, index=count.index)
-            self.result_df = result_df1.replace(0, np.nan)
-
-        return self.result_df
+    #     df1.columns = newheaders
+    #     self.result_df2 = df1
+        
+    #     return self.result_df2
     
-    def fix_col_headers(self):
-        df1 = self.result_df
-        dfcols = self.result_df.columns
-        newheaders=[]
-        for i in dfcols:
-            header =  i.split('_',1)[1]
-            newheaders.append(header)
-            
-        df1.columns = newheaders
-        self.result_df2 = df1
+    # def create_group_dfs(self):
         
-        return self.result_df2
-    
-    def create_group_dfs(self):
+    #     x=self.result_df2.map(lambda x: (x <= 150))
+    #     y=self.result_df2.map(lambda x: (x >  150))
+    #     self.countA = x.sum(axis=1)
+    #     self.countB = y.sum(axis=1)
         
-        x=self.result_df2.map(lambda x: (x <= 150))
-        y=self.result_df2.map(lambda x: (x >  150))
-        self.countA = x.sum(axis=1)
-        self.countB = y.sum(axis=1)
+    #     return self.countA, self.countB
         
-        return self.countA, self.countB
-        
-    def create_monthly(self):
+    # def create_monthly(self):
 
-        year                = self.countA.index.year
-        month               = self.countB.index.month
-        self.countA_monthly = self.countA.groupby([year, month]).sum()
-        self.countB_monthly = self.countB.groupby([year, month]).sum()
+    #     year                = self.countA.index.year
+    #     month               = self.countB.index.month
+    #     self.countA_monthly = self.countA.groupby([year, month]).sum()
+    #     self.countB_monthly = self.countB.groupby([year, month]).sum()
         
-        self.all_monthly    = self.df_results.groupby([year, month]).sum()
+    #     self.all_monthly    = self.df_results.groupby([year, month]).sum()
         
-        return self.countA_monthly, self.countB_monthly, self.all_monthly
+    #     return self.countA_monthly, self.countB_monthly, self.all_monthly
 
 
 
     
-    def get_dash_vars(self):
-        self.days_of_milking_dash_vars = {name: value for name, value in vars(self).items()
-               if isinstance(value, (pd.DataFrame, pd.Series))}
-        return self.days_of_milking_dash_vars  
+    # def get_dash_vars(self):
+    #     self.days_of_milking_dash_vars = {name: value for name, value in vars(self).items()
+    #            if isinstance(value, (pd.DataFrame, pd.Series))}
+    #     return self.days_of_milking_dash_vars  
     
-    def write_to_csv(self):
-        self.result_df.to_csv('F:\\COWS\\data\\milk_data\\lactations\\daysOfMilking_daily.csv')
-        self.all_monthly.to_csv('F:\\COWS\\data\\milk_data\\lactations\\daysOfMilking_monthly.csv')
-        self.countA_monthly.to_csv('F:\\COWS\\data\\milk_data\\lactations\\countA_monthly.csv')
-        self.countB_monthly.to_csv('F:\\COWS\\data\\milk_data\\lactations\\countB_monthly.csv')
+    # def write_to_csv(self):
+    #     self.result_df.to_csv('F:\\COWS\\data\\milk_data\\lactations\\daysOfMilking_daily.csv')
+    #     self.all_monthly.to_csv('F:\\COWS\\data\\milk_data\\lactations\\daysOfMilking_monthly.csv')
+    #     self.countA_monthly.to_csv('F:\\COWS\\data\\milk_data\\lactations\\countA_monthly.csv')
+    #     self.countB_monthly.to_csv('F:\\COWS\\data\\milk_data\\lactations\\countB_monthly.csv')
 
 
 
