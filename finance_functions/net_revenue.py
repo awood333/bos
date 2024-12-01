cf1a7bc3f1f6e550c7eb554cf86a9bea3b0b11a4
alan woodhull <89449956+awood333@users.noreply.github.com>
2024-12-02 05:33:35 +0700

    12/02

--- finance_functions/net_revenue.py
+++ finance_functions/net_revenue.py
@@ -2,11 +2,19 @@
 
 import pandas as pd
 import numpy as np
+import warnings
+
 from finance_functions.milk_income import MilkIncome
 from feed_functions.feed_cost_basics import FeedCostBasics
 from milk_functions.WetDry          import WetDry
 from milk_functions.statusData     import StatusData
 from milk_functions.status_groups   import statusGroups
+
+warnings.simplefilter('always', FutureWarning)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
 class NetRevenue:
     def __init__ (self):
@@ -22,6 +30,8 @@ class NetRevenue:
         self.write_to_csv()
         
     def compare_lists(self):
+        
+        # from status_groups
         a = self.SG.group_A_ids.iloc[-1:,1:].values
         b = self.SG.group_B_ids.iloc[-1:,1:].values
         f = self.SG.fresh_ids.iloc[-1:,1:].values
@@ -30,44 +40,69 @@ class NetRevenue:
         status_groups_list3 = status_groups_list2.T
         status_groups_list = status_groups_list3.sort_values(by=status_groups_list3.columns[0]).reset_index(drop=True)
         
-        m = pd.DataFrame(self.SD.milker_ids[-1])
         
+        # from status_data
+        m = pd.DataFrame(np.array(self.SD.milker_ids[-1]))
+        
+        
+        # merge 
         self.milker_lists_comp = pd.merge(status_groups_list, m, left_index=True, right_index=True)
+        if len(m) != len(status_groups_list):
+            print("look at F:\\COWS\\data\\status\\milker_lists_comp.csv")
 
         return self.milker_lists_comp
         
 
     def create_net_revenue(self):
+        
 
-        nr = pd.concat((self.SG.group_A, self.SG.group_B, self.SD.herd_daily, self.FCB.feedcost_daily ), axis=1)
-        nr = pd.concat((nr, self.MI.income_daily['daily_avg_net']), axis=1)
+        nr1 = pd.concat((self.SG.group_A, self.SG.group_B, self.SG.fresh, self.SD.herd_daily, self.FCB.feedcost_daily ), axis=1)
+        nr2 = pd.concat((nr1, self.MI.income_daily['daily_avg_net']), axis=1)
+        nr2['days'] = nr2.index.days_in_month
         
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_columns', None)
-        pd.set_option('display.width', None)
-        pd.set_option('display.max_colwidth', None)
-        print('nr ', nr.iloc[:5,:])
-        
-        nr['A_agg_cost'] = nr['groupA_count'] * nr['cost A'] * nr.index.get_level_values('days')
-        nr['B_agg_cost'] = nr['groupB_count'] * nr['cost B'] * nr.index.get_level_values('days')
-        nr['total cost'] = nr['A_agg_cost'] + nr['B_agg_cost'] + nr['dry 15pct agg cost']
-        nr['net revenue'] = nr['gross_baht'] - nr['total cost']
-        
-        nr = nr.drop(columns=(['milkers', 'milkers agg cost']))
 
-        self.net_revenue = nr.loc[:,[
-                             
-                            'gross_baht',
+        # print('nr2 ', nr2.iloc[:5,:])
+
+        nr2['fresh_agg_cost']   = nr2['fresh']      * nr2['totalcostA'] 
+        nr2['A_agg_cost']       = nr2['groupA']     * nr2['totalcostA'] 
+        nr2['B_agg_cost']       = nr2['groupB']     * nr2['totalcostB'] 
+        nr2['D15_agg_cost']     = nr2['dry_15pct']  * nr2['totalcostD'] 
+        
+        nr2['total cost'] = nr2['fresh_agg_cost'] + nr2['A_agg_cost'] + nr2['B_agg_cost'] + nr2['D15_agg_cost']
+        nr2['net revenue'] = nr2['daily_avg_net'] - nr2['total cost']
+        
+        # nr2 = nr2.drop(columns=(['milkers', 'milkers agg cost']))
+        nr3 = nr2.rename(columns={
+            'groupA' : "A count",
+            'groupB' : 'B count',
+            'fresh'  : 'fresh count',
+            'dry'     : 'dry_all count',
+            'dry_15pct' : 'D15 count',
+            'totalcostA' : "cost A",
+            'totalcostB' : "cost B",
+            'totalcostD' : "cost D",
+            'daily_avg_net' : 'baht'
+            
+            
+        }) 
+
+        self.net_revenue = nr3.loc[:,[
+                            'milkers',
+                            'fresh count',
+                            'A count',
+                            'B count',
+                            'dry_all count',
+                            'D15 count',
+                            'alive',
                             'cost A',
                             'cost B',
-                            'dry cost',
-                            'groupA_count',
-                            'groupB_count',
-                            'dry_15pct',
+                            'cost D',
+                            'fresh_agg_cost',
                             'A_agg_cost',
                             'B_agg_cost',
-                            'dry 15pct agg cost',
+                            'D15_agg_cost',
                             'total cost',
+                            'baht',                            
                             'net revenue'
             ]
         ]
@@ -77,5 +112,7 @@ class NetRevenue:
     def write_to_csv(self):
         self.net_revenue.to_csv('F:\\COWS\\data\\feed_data\\feedcost_by_group\\net.csv')
         self.milker_lists_comp.to_csv('F:\\COWS\\data\\status\\milker_lists_comp.csv')
+        
+        
 if __name__ == "__main__":
     NetRevenue()            
