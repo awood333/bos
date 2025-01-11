9c79de5aafcf2d79dfcbb20032f3a0fecef24997
alan woodhull <89449956+awood333@users.noreply.github.com>
2025-01-11 12:26:38 +0700

    1/11

--- feed_functions/heifers.py
+++ feed_functions/heifers.py
@@ -4,24 +4,26 @@ import pandas as pd
 import numpy as np
 from datetime import datetime,timedelta
 from feed_functions.feed_cost_basics import FeedCostBasics
-
+from insem_functions.Insem_ultra_data import InsemUltraData
 
 class Heifers:
     def __init__(self):
 
         self.FCB = FeedCostBasics()
+        self.IUD = InsemUltraData()
         self.today = datetime.now()
         self.rng = pd.date_range('2023-01-01', self.today)
+ 
         
-   
-        
-        self.heifers, self.WY_ids,        = self.get_heifer_age()
+        self.heifers, self.WY_ids,        = self.create_heifer_df()
         self.days3, self.date_col       = self.create_heifer_days()
         
-        self.milk_drinking = self.calc_milkdrinking_cost()
+        self.milk_drinking_days = self.calc_milkdrinking_days()
+        self.TMR_amt_days = self.calc_TMR_days()
+        self.yellow_beans_amt_days = self.create_yellow_beans_days()
         
         
-    def get_heifer_age(self):
+    def create_heifer_df(self):
         today = datetime.now()
         heifers1 = pd.read_csv("F:\\COWS\\data\\csv_files\\heifers.csv", 
             header=0, index_col=None)
@@ -36,14 +38,14 @@ class Heifers:
     def create_heifer_days(self):
         heifer_age3 = pd.DataFrame(
             columns = self.heifers['WY_id'],
-            index=  self.rng
+            index=  self.rng                    #creates an array with WY_id cols and datex index
             )
         
         self.date_col = heifer_age3.index.to_list()      
-       
         days2 = days3 = pd.DataFrame()
+        WY_ids_integer = [int(i) for i in self.WY_ids]
         
-        for i in self.WY_ids:
+        for i in WY_ids_integer:
             for j in self.date_col:
                 start = self.heifers['b_date'][i-1]
                 stop = self.today
@@ -60,8 +62,9 @@ class Heifers:
         self.days = days3
         
         return self.days,  self.date_col
+
     
-    def calc_milkdrinking_cost(self):
+    def calc_milkdrinking_days(self):
         milk_drinking2=[]
         milk_drinking3 = [] #pd.DataFrame(columns=['WY_id'])
         for i in self.WY_ids:
@@ -72,35 +75,60 @@ class Heifers:
             milk_drinking1b = len(milk_drinking1a)
             milk_drinking2.append(milk_drinking1b)
             
-            start_TMR1 = days[days == 100].index
-            stop_TMR_growth1  = days[days == 360].index
-            
-            start_TMR   = start_TMR1[0]
-            stop_TMR_growth1 = stop_TMR_growth1[0]
-            
-            start_TMR_regular = stop_TMR_growth1 + timedelta(days=1)
- 
-            
-            preg_date1   = self.heifers['preg_date'][i-1]
-            preg_date   = pd.to_datetime(preg_date1)
-            
-            TMR_regular_range = pd.date_range(start_TMR_regular,preg_date)
-            
-            TMR_growth_range = pd.date_range(start_TMR, stop_TMR_growth1)
-            len_TMR_growth_range = len(TMR_growth_range)
-            TMR_growth_series = np.linspace(2, 20, len_TMR_growth_range)
-            
-            TMR_regular_series = np.full(len(TMR_regular_range), 20)
-            TMR_series = np.concatenate([TMR_growth_series,TMR_regular_series])
-            
-            
-            yellow_bean1a = days.loc[(days > 100)].dropna()
-            
-            
         milk_drinking3.append(milk_drinking2)
-        self.milk_drinking = pd.DataFrame(milk_drinking3, columns=['WY_ids'])
+        self.milk_drinking_days = pd.DataFrame(milk_drinking3, columns=[self.WY_ids])
             
-        return self.milk_drinking
+        return self.milk_drinking_days
+    
+    def calc_TMR_days(self):
+        
+        for i in self.WY_ids:
+            
+            days = self.days[i]
+            preg_date = self.heifers['preg_date'][i]
+            
+            start_TMR_growth1 = days[days == 90].index
+            stop_TMR_growth1  = days[days == 365].index
+            
+            start_TMR_growth   = start_TMR_growth1[0]
+            stop_TMR_growth = stop_TMR_growth1[0]
+            
+            start_TMR_growth_regular = stop_TMR_growth + timedelta(days=1)
+ 
+            if preg_date:
+                end_date1   = self.heifers['preg_date'][i-1]
+                end_date   = pd.to_datetime(end_date1)
+            
+            elif not preg_date:
+                end_date = self.today
+            
+            
+            
+            # these create an array of the amounts of TMR/day over the date range
+
+            TMR_growth_range = pd.date_range(start_TMR_growth, stop_TMR_growth)
+            len_TMR_growth_range = len(TMR_growth_range)
+            TMR_growth_amt_series = np.linspace(.5, 20, len_TMR_growth_range)
+            
+            TMR_regular_range = pd.date_range(start_TMR_growth_regular,end_date)
+            TMR_regular_amt_series = np.full(len(TMR_regular_range), 20)
+            
+            self.TMR_amt_days = np.concatenate([TMR_growth_amt_series,TMR_regular_amt_series])
+            
+        return self.TMR_amt_days   
+    
+    def create_yellow_beans_days(self):
+        
+        for i in self.WY_ids:
+            days = self.days[i]
+            start_yellow_beans = days[days == 120].index
+            stop_yellow_beans = days[days == 150].index
+            yellow_beans_days = pd.date_range(start_yellow_beans, stop_yellow_beans)
+            self.yellow_beans_amt_days = np.full(len(yellow_beans_days, 1))
+            
+        return self.yellow_beans_amt_days
+            
+            
         
         
         
