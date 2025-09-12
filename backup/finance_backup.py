9f4ec4587b49a53ebc874ed48c7ea518a8028d0c
alan woodhull <89449956+awood333@users.noreply.github.com>
2025-09-12 07:34:15 +0700

    9/12

--- backup/finance_backup.py
+++ backup/finance_backup.py
@@ -14,7 +14,7 @@ class FinanceDataBackup:
     def write_to_csv(self):
         bkkbank = pd.read_csv('F:\\COWS\\data\\finance\\BKKbank\\BKKBankFarmAccount.csv')
         depr    = pd.read_csv('F:\\COWS\\data\\finance\\capex\\depreciation_schedule\\depreciation_schedule.csv')
-        milk_inc= pd.read_csv("F:\\COWS\\data\PL_data\\milk_income\\data\\milk_income_data.csv")
+        milk_inc= pd.read_csv("F:\\COWS\\data\\PL_data\\milk_income\\data\\milk_income_data.csv")
         
         bkkbank .to_csv(f'F:\\COWS\\data\\finance\\BKKbank\\backup\\BKKBankFarmAccount_{tdy}.csv')
         bkkbank .to_csv(f'E:\\Cows\\data_backup\\finance_backup\\farm_account\\BKKBankFarmAccount_{tdy}.csv')
